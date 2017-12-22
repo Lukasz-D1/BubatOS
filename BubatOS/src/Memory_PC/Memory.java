f76c27f07cf4e366200ac10d55e9767212f35f75
przemyslawczajka <32740813+przemyslawczajka@users.noreply.github.com>
2017-12-22 12:56:37 +0100

    Ustatycznienie klasy, poprawa mniejszych błędów
    Słowo „final” jest dla klasy odpowiednikiem słowa „static”

--- BubatOS/src/Memory_PC/Memory.java
+++ BubatOS/src/Memory_PC/Memory.java
@@ -1,21 +1,40 @@
 package Memory;
 
-public class Memory {
+public final class Memory {
 
-	private short addr[] = { -1, -1, -1, -1 }; //adresy logicznej stron znajdujących się w poszczególnych ramkach
-	private byte ws[] = { -1, -1, -1 }; //working set – zbiór roboczy
-	private byte current; //indeks aktualnie rozpatrywanego elemntu zbioru roboczego
-	private char frames[]; //ramki
-	VirtualMemory vm;
+	private static short addr[] = { -1, -1, -1, -1 }; //adresy logiczne stron znajdujących się w poszczególnych ramkach
+	private static byte ws[] = { -1, -1, -1 }; //working set – zbiór roboczy
+	private static byte current; //indeks aktualnie rozpatrywanego elemntu zbioru roboczego
+	private static char frames[]; //ramki
 
 	Memory() {
 		frames = new char[64];
-		vm = new VirtualMemory();
 		current = 0;
 	}
 
+	static void wite(byte p, byte d, char[] data) {
+		for (byte i = 0; i != 4; ++i) {
+			if (addr[ws[i]] == p) {
+				ws[current] = i;
+				++current;
+				current %= 3;
+				for (byte j = 0; j != data.length + d; ++j) {
+					frames[ws[i] * 16 + d + j] = data[j];
+				}
+				return;
+			}
+		}
+		load(p);
+		++current;
+		current %= 3;
+		for (byte j = 0; j != data.length + d; ++j) {
+			frames[ws[current-1] * 16 + d + j] = data[j];
+		}
+		return;
+	}
+	
 	//funkcja podająca n znaków, zaczynając od strony o adresie logicznym p i offsecie d
-	char[] read(byte p, byte d, byte n) {
+	static char[] read(byte p, byte d, byte n) {
 		char ret[] = new char[n];
 		for (byte i = 0; i != 4; ++i) {
 			if (addr[ws[i]] == p) {
@@ -32,7 +51,7 @@ public class Memory {
 		++current;
 		current %= 3;
 		for (byte j = d; j != n + d; ++j) {
-			ret[j] = frames[ws[0] * 16 + j];
+			ret[j] = frames[ws[current-1] * 16 + j];
 		}
 		return ret;
 	}	
@@ -48,7 +67,7 @@ public class Memory {
 		return ret;
 	}
 	
-	private void load(short p) {
+	private static void load(short p) {
 		byte i = 0;
 		while (i != 4) {
 			if (absence(i)) {
@@ -57,16 +76,16 @@ public class Memory {
 		}
 		if (addr[ws[current]] != -1) {
 			for (i = 0; i != 16; ++i) {
-				vm.pages[p][i] = frames[ws[3] * 16 + i];
+				VirtualMemory.pages[p][i] = frames[ws[current] * 16 + i];
 			}
 		}
 		for (i = 0; i != 16; ++i) {
-			frames[ws[current] * 16 + i] = vm.pages[p][i];
+			frames[ws[current] * 16 + i] = VirtualMemory.pages[p][i];
 		}
 	}
 
-
-	private Boolean absence(byte d) {
+	
+	private static Boolean absence(byte d) {
 		byte i = 0;
 		while (i != 3) {
 			if (ws[i] == d)

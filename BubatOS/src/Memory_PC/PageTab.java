86888ce6f6197c2565ae2b563210ab8a03c855bf
przemyslawczajka <32740813+przemyslawczajka@users.noreply.github.com>
2017-12-27 21:56:01 +0100

    Poprawa błędów

--- BubatOS/src/Memory_PC/PageTab.java
+++ BubatOS/src/Memory_PC/PageTab.java
@@ -1,27 +1,25 @@
 package Memory;
 
 public class PageTab {
-	byte[] tab; // tablica indeksów stron, w których są dane procesu
+	byte[] tab; // tablica numerów stron, w których są dane procesu
 
 	PageTab(char data[]) {
-		tab = VirtualMemory.load(data);
+		tab = MassMemory.load(data);
 	}
 
 	protected void finalize() {
-		VirtualMemory.clear(tab);
+		MassMemory.clear(tab);
 	}
-	
-	//Poniżej są metody pozwalające pozyskiwać i zapisywać dane z/w pamięci przydzielonej procesowi
-	
-	//metoda odczytująca amount znaków zaczynając od adresu ad
-	char[] read(short ad, short amount) {
-		if (ad + amount >= tab.length * 16) { // Gdy odwołano się do znaku o zbyt dużym adresie
+
+	// metoda odczytująca amount znaków zaczynając od adresu ad
+	char[] read(int j, int amount) {
+		if (j + amount >= tab.length * 16) { // Gdy odwołano się do znaku o zbyt dużym adresie
 			return null;
 		}
 		char[] ret = new char[amount];
-		if ((ad % 16) + amount > 32) { // odczytywanie z trzech stron
-			byte p = tab[ad / 16];
-			byte d = (byte) (ad % 16);
+		if ((j % 16) + amount > 32) { // odczytywanie z trzech stron
+			byte p = tab[j / 16];
+			byte d = (byte) (j % 16);
 			byte n = (byte) (16 - d);
 			char part[] = Memory.read(p, d, n);
 			byte re = 0;
@@ -29,7 +27,7 @@ public class PageTab {
 				ret[i] = part[i];
 			}
 			re += n;
-			p = tab[ad / 16 + 1];
+			p = tab[j / 16 + 1];
 			d = 0;
 			n = 16;
 			part = Memory.read(p, d, n);
@@ -37,16 +35,16 @@ public class PageTab {
 				ret[re + i] = part[i];
 			}
 			re += n;
-			p = tab[ad / 16 + 2];
+			p = tab[j / 16 + 2];
 			d = 0;
-			n = (byte) (amount-re);
+			n = (byte) (amount - re);
 			part = Memory.read(p, d, n);
 			for (byte i = 0; i < n; ++i) {
 				ret[re + i] = part[i];
 			}
-		} else if ((ad % 16) + amount > 16) { // odczytywanie z dwóch stron
-			byte p = tab[ad / 16];
-			byte d = (byte) (ad % 16);
+		} else if ((j % 16) + amount > 16) { // odczytywanie z dwóch stron
+			byte p = tab[j / 16];
+			byte d = (byte) (j % 16);
 			byte n = (byte) (16 - d);
 			char part[] = Memory.read(p, d, n);
 			byte re = 0;
@@ -54,18 +52,17 @@ public class PageTab {
 				ret[i] = part[i];
 			}
 			re += n;
-			p = tab[ad / 16 + 1];
+			p = tab[j / 16 + 1];
 			d = 0;
-			n = (byte) (amount-re);
+			n = (byte) (amount - re);
 			part = Memory.read(p, d, n);
 			for (byte i = 0; i < n; ++i) {
 				ret[re + i] = part[i];
 			}
-		}
-		else { //odczytywanie z jednej strony
-			byte p = tab[ad / 16];
-			byte d = (byte) (ad % 16);
-			byte n = (byte) (16 - d);
+		} else { // odczytywanie z jednej strony
+			byte p = tab[j / 16];
+			byte d = (byte) (j % 16);
+			byte n = (byte) (amount);
 			char part[] = Memory.read(p, d, n);
 			for (byte i = 0; i < n; ++i) {
 				ret[i] = part[i];
@@ -73,16 +70,15 @@ public class PageTab {
 		}
 		return ret;
 	}
-	
+
 	// metoda zapisująca znaki data zaczynając od adresu ad
-	void write(short ad, char[] data) {
-		if (ad + data.length >= tab.length * 16) { // Gdy odwołano się do znaku o zbyt dużym adresie
+	void write(int j, char[] data) {
+		if (j + data.length >= tab.length * 16) { // Gdy odwołano się do znaku o zbyt dużym adresie
 			return;
 		}
-		char[] ret = new char[data.length];
-		if ((ad % 16) + data.length > 32) { // zapisywanie na trzech stronach
-			byte p = tab[ad / 16];
-			byte d = (byte) (ad % 16);
+		if ((j % 16) + data.length > 32) { // zapisywanie na trzech stronach
+			byte p = tab[j / 16];
+			byte d = (byte) (j % 16);
 			byte n = (byte) (16 - d);
 			char[] part = new char[n];
 			for (byte i = 0; i < n; ++i) {
@@ -91,7 +87,7 @@ public class PageTab {
 			Memory.write(p, d, part);
 			byte wr = 0;
 			wr += n;
-			p = tab[ad / 16 + 1];
+			p = tab[j / 16 + 1];
 			d = 0;
 			n = 16;
 			part = new char[n];
@@ -100,7 +96,7 @@ public class PageTab {
 			}
 			Memory.write(p, d, part);
 			wr += n;
-			p = tab[ad / 16 + 2];
+			p = tab[j / 16 + 2];
 			d = 0;
 			n = (byte) (data.length - wr);
 			part = new char[n];
@@ -108,9 +104,9 @@ public class PageTab {
 				part[i] = data[wr + i];
 			}
 			Memory.write(p, d, part);
-		} else if ((ad % 16) + data.length > 16) { // zapisywanie na dwóch stronach
-			byte p = tab[ad / 16];
-			byte d = (byte) (ad % 16);
+		} else if ((j % 16) + data.length > 16) { // zapisywanie na dwóch stronach
+			byte p = tab[j / 16];
+			byte d = (byte) (j % 16);
 			byte n = (byte) (16 - d);
 			byte wr = 0;
 			char[] part = new char[n];
@@ -119,7 +115,7 @@ public class PageTab {
 			}
 			Memory.write(p, d, part);
 			wr += n;
-			p = tab[ad / 16 + 1];
+			p = tab[j / 16 + 1];
 			d = 0;
 			n = (byte) (data.length - wr);
 			part = new char[n];
@@ -128,11 +124,11 @@ public class PageTab {
 			}
 			Memory.write(p, d, part);
 		} else { // zapisywanie na jednej stronie
-			byte p = tab[ad / 16];
-			byte d = (byte) (ad % 16);
-			byte n = (byte) (16 - d);
+			byte p = tab[j / 16];
+			byte d = (byte) (j % 16);
+			byte n = (byte) (data.length);
 			char[] part = new char[n];
-			for (byte i = 0; i < n; ++i) {
+			for (byte i = 0; i != n; ++i) {
 				part[i] = data[i];
 			}
 			Memory.write(p, d, part);

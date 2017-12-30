77aba661567cea824478dce3dfce6a9a92a5d367
arturb1922 <32738777+arturb1922@users.noreply.github.com>
2017-12-30 23:00:06 +0100

    Update Semaphore.java

--- BubatOS/src/semaphore/Semaphore.java
+++ BubatOS/src/semaphore/Semaphore.java
@@ -1,14 +1,22 @@
+//ZMIANY
+// Pozmieniałem wątki na procesy kierując się klasą Łukasza, dodałem też planiste i tu jescze nie jestem w 100% 
+//pewnien czy to tak ma byc, ale jeszcze przeprowadze z Kuba dokładną rozmowę, bo akurat kiedy to robiłem to nie mógł gadać. 
+// Na moje to już jest tak prawie 100%, ale znając życie jest z 20% zrobione i prędzej dostane depresji niż to zacznie działać
+// Jak coś to pisać
+
 package semaphore;
 
 import java.util.LinkedList;
+import ProcessManagment.Process;
+import CPU_Scheduling.Scheduler;
 
 public class Semaphore {
 private boolean stan;
 
 public String name;
-public LinkedList<Thread> WaitingList= new LinkedList <Thread>();
-public LinkedList <Thread> ReadyList = new LinkedList<Thread>();
-public boolean IsUsed;
+public LinkedList<Process> WaitingList= new LinkedList <Process>();
+//public LinkedList <Process> ReadyList = new LinkedList<Process>();
+
 
 public Semaphore( String name) {
 	this.stan=true;
@@ -19,63 +27,48 @@ public boolean isStan() {
 	return stan;
 }
 
-public void P (Thread t) throws InterruptedException
+public void P (Process p) throws InterruptedException
 {
+	Scheduler p1=new Scheduler();
 	if( stan==true)
 	{
-		IsUsed=false;
-		IsUsed=true;
 		System.out.println("Semafor jest juz podniesiony. Proces przechodzi dalej");
 		stan=false; // semafor podniesiony, proces opuszcza semafor i wykonuje critical section 
-		System.out.println("Proces" + t.getName() +" wykonuje sie");
-		t.sleep(2000);
+		System.out.println("Proces" + p.getProcessName() +" wykonuje sie");
 	}
 	else
 	{
 		System.out.println("Semafor jest aktualnie opuszczony. Proces nie moze dzialac dalej.");
 		System.out.println("Proces zmienia stan na oczekujacy");
-		WaitingList.add(t);
+		p.setStan(Process.processState.Waiting);
+		WaitingList.add(p);
+		p1.ReadyThread(p,true);
 	}
 }
 
-public void V(Thread t) throws InterruptedException
+public void V() throws InterruptedException
 {
-
+	Scheduler p1=new Scheduler();
 	if(stan==false)
 	{
 		if(WaitingList.isEmpty()==false)
 		{
-			IsUsed=false;
-			t=WaitingList.getFirst();
-			System.out.println("Proces" + t.getName()+" zmienia status na gotowy");
-			ReadyList.add(t);
+			Process p;
+			p=WaitingList.getFirst();
+			System.out.println("Proces" + p.getProcessName()+" zmienia status na gotowy\n i opuszcza kolejke procesow oczekujacych");
+			p.setStan(Process.processState.Ready);
+			//ReadyList.add(p);
 			WaitingList.removeFirst();
-			IsUsed=true;
-			stan=true;
-			P(t);
+			p1.ReadyThread(p,true);
 		}
 		else {
 			System.out.println("Brak procesow oczekujacych. Podniesienie semafora");
 			stan=true;
-			P(t);
 		}
 	}
 	else {System.out.println("Semafor jest juz aktualnie podniesiony");}
 }
 
-	
-//to dla Adama, ale ogólnie patrząc na systemy innych to oni jakoś tego nie mieli
-//i chyba samymi P i V się można porozumiewać z tym
-public boolean isIsUsed(Thread t) {
-	if(IsUsed==true)
-	{
-		System.out.println("Plik jest aktualnie uzywany.");
-		return IsUsed;
-	}
-	else{
-		System.out.println("Plik nie jest aktualnie używany");
-		return IsUsed;
-	}
 }
 
-}
+

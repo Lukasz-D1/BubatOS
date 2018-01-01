6582bab6a51ebf6e127efef114d8776a82283d13
chradam <32743926+chradam@users.noreply.github.com>
2018-01-01 22:25:54 +0100

    Update FileEntry.java

--- BubatOS/src/FileSystem/FileEntry.java
+++ BubatOS/src/FileSystem/FileEntry.java
@@ -13,22 +13,10 @@ public class FileEntry {
 	        return this.typeCode;
 	    }
 	}
-	//protected static final int MAX_FILE_SIZE = 40; //B
 	String name;
-	//String ext;
-	//String location; //po³a¿enie wskaznik /Users/greg/text.txt
-	int inodeNum; //ideks do tablicy i-wêz³ów
-	Types type_of_file; //dowi¹zanie czy zwyk³y plik ????????????????? potrzebne przy dowi¹zaniu
-	//int adrIndexBlock;
-	//boolean stan; //czy otwarty true-uzywany przez proces
-	//int size; //B
+	int inodeNum; //indeks do tablicy i-wÄ™zÅ‚Ã³w
+	Types type_of_file;
 	//w momencie czytanie umieszczany na poczatku pliku
 	//w momencie zapisu na koncu pliku
 	int currentPositionPtr;//pozycja czytania w pliku
-	//obiekt semafora
-	//Semaphore s;
-	//protected int [] atrybuty = new int[10];//trwxrwxrwx pierwszy okresla typ(zwyk³y czy katalog)
-									//x np.pozwala zmienic biezacy katalog
-	//blok indeskowy
 }
-

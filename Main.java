e22e1f2b3095255bf0a187fdfcaa8354972b7981
BMarcin <BMarcin@users.noreply.github.com>
2018-01-03 20:49:31 +0100

    Poprawka by działało z resztą

---
+++ Main.java
@@ -0,0 +1,27 @@
+package ProcessManagment;
+
+public class Main {
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		/*ProcessManagment p1 = new ProcessManagment();
+		
+		
+		System.out.println(p1.mainProcess.getProcessName());
+		
+		Process jeden = p1.fork(p1.mainProcess);
+		System.out.println(jeden.getState());
+		jeden.setProcessName("pierwszy");
+		
+		Process dwa = p1.fork(jeden);
+		dwa.setProcessName("drugi");
+		
+		Process trzy = p1.fork(jeden);
+		trzy.setProcessName("trzeci");
+		
+		Process cztery = p1.getProcessByName("drugi");
+		System.out.println(cztery.getProcessName());
+		
+		jeden.showProcessChildrenList();
+		p1.ps();*/
+	}
+}
